621dc8ad7755b90e6d8ab85fa157618d856d8a63
Timothy Tentinger <ttentinger@gmail.com>
2016-07-06 00:47:56 -0400

    Still messing around and around and around

--- app/src/main/java/com/fynydd/firsttut/MainActivity.java
+++ app/src/main/java/com/fynydd/firsttut/MainActivity.java
@@ -2,7 +2,9 @@ package com.fynydd.firsttut;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+//
 // Just adding a comment
+//
 public class MainActivity extends AppCompatActivity {
 
     @Override
